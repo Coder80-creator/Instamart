b9a350d271a03d7d869a98f90ae5ab85fbd255c6
Coder80-creator <142227331+Coder80-creator@users.noreply.github.com>
2023-08-19 11:27:51 +0530

    Update and rename chat1.js to chat2.js

--- chat2.js
+++ chat2.js
@@ -105,7 +105,7 @@ async function word_count(){
     temp=w.replace(len.toLowerCase(),`<span>${len}</span>`);
     word.innerHTML=temp;
     word.onclick=function() {
-      //alert(77);
+      alert(77);
     set_cookie(mode_,w);
     };
     let parent=document.querySelector("#suggestions");
@@ -168,7 +168,7 @@ async function get_sugg(data) {
 
 
 function set_cookie(mode,search){
-  //alert("yes");
+  alert("yes");
   
   document.cookie = `mode=${mode}; expires=Thu, 18 Dec 2025 12:00:00 UTC,domain=localhost`;
   document.cookie = `search=${search}; expires=Thu, 18 Dec 2025 12:00:00 UTC,domain=localhost`;
@@ -177,7 +177,9 @@ function set_cookie(mode,search){
 }
 
 function refreshPage() {
-            window.location.reload();
+            //window.location.reload();
+            document.getElementById("checkbox").checked=true;
+            count=0;
         }
 
         // Add an event listener to detect when the page regains focus
